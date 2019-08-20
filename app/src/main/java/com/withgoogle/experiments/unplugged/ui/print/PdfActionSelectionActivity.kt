38a534781799a14982719f55b2b604a652153f39
Gonçalo Ferreira <goncalo@waterdog.mobi>
2019-08-20 18:51:48 +0100

    Implement document drawing with Android Print Framework

--- app/src/main/java/com/withgoogle/experiments/unplugged/ui/print/PdfActionSelectionActivity.kt
+++ app/src/main/java/com/withgoogle/experiments/unplugged/ui/print/PdfActionSelectionActivity.kt
@@ -1,34 +1,66 @@
 package com.withgoogle.experiments.unplugged.ui.print
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.print.*
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import com.withgoogle.experiments.unplugged.R
 import com.withgoogle.experiments.unplugged.ui.AppState
-import com.withgoogle.experiments.unplugged.ui.pdf.PdfGenerator
+import com.withgoogle.experiments.unplugged.ui.pdf.*
 import com.withgoogle.experiments.unplugged.util.bindView
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import java.io.File
 
 class PdfActionSelectionActivity: AppCompatActivity() {
     private val savePdf by bindView<Button>(R.id.save_pdf)
+    private val printPdf by bindView<Button>(R.id.print)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pdf_action)
 
         findViewById<Button>(R.id.print).setOnClickListener {
-            startActivity(Intent(this, FoldingInfoActivity::class.java))
+            it.isEnabled = false
+            printPdf()
         }
 
         findViewById<Button>(R.id.save_pdf).setOnClickListener {
             it.isEnabled = false
             savePdf()
+        }
+
+        findViewById<Button>(R.id.print_info).setOnClickListener {
+            startActivity(Intent(this, FoldingInfoActivity::class.java))
+        }
+    }
+
+    private fun printPdf() {
+        val printManager = getSystemService(Context.PRINT_SERVICE) as PrintManager
+        val jobName = "${getString(R.string.app_name)} Document"
+
+        AppState.modules.value?.let { modules ->
+            CoroutineScope(Dispatchers.Main).launch {
+                withContext(Dispatchers.IO) {
+                    modules.first.forEach { it.setupData() }
+                    modules.second?.setupData()
+                }
+
+                printManager.print(jobName, PaperPhoneDocumentAdapter(
+                    this@PdfActionSelectionActivity,
+                    modules.first, modules.second, modules.third
+                ), PrintAttributes.Builder()
+                    .setMediaSize(PrintAttributes.MediaSize.ISO_A4.asLandscape())
+                    .setColorMode(PrintAttributes.COLOR_MODE_MONOCHROME)
+                    .setResolution(PrintAttributes.Resolution("best","best", 300, 300))
+                    .setMinMargins(PrintAttributes.Margins.NO_MARGINS)
+                    .build())
+
+                printPdf.isEnabled = true
+            }
+
         }
     }
 
