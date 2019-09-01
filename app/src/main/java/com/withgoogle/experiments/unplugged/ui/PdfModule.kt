c2ab6ff5a60f58c144e0c96bdab6774c143b7a89
Gonçalo Ferreira <goncalo@waterdog.mobi>
2019-09-01 22:23:46 +0100

    PdfModule documentation

--- app/src/main/java/com/withgoogle/experiments/unplugged/ui/PdfModule.kt
+++ app/src/main/java/com/withgoogle/experiments/unplugged/ui/PdfModule.kt
@@ -4,8 +4,18 @@ import android.content.res.Resources
 import android.graphics.Canvas
 
 interface PdfModule {
+    /**
+     * Drawing step, runs on the UI thread
+     */
     fun draw(canvas: Canvas, resources: Resources)
+
+    /**
+     * Attribute to draw this module upside down
+     */
     val isRotated: Boolean
 
+    /**
+     * Fetch data step. This is run on a background
+     */
     suspend fun setupData()
 }
